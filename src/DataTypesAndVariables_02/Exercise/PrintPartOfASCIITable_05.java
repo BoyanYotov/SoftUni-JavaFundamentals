c500e2c88d38fcb9cc19c27888c3bf316f4c98d3
BoyanYotov <bojan.jotov@gmail.com>
2025-01-18 10:35:11 +0200

    Finished task 5

--- src/DataTypesAndVariables_02/Exercise/PrintPartOfASCIITable_05.java
+++ src/DataTypesAndVariables_02/Exercise/PrintPartOfASCIITable_05.java
@@ -1,4 +1,17 @@
 package DataTypesAndVariables_02.Exercise;
 
 public class PrintPartOfASCIITable_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startCharacter = Integer.parseInt(scanner.nextLine());
+        int endCharacter = Integer.parseInt(scanner.nextLine());
+
+        for (int i = startCharacter; i <= endCharacter ; i++) {
+            char symbol = (char) i;
+
+            System.out.print(symbol + " ");
+        }
+
+    }
 }
